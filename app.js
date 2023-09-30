c847e33f3c490208d583a7c98f5afab422f164db
Guilherme <98475125+FireguiQueen@users.noreply.github.com>
2023-09-29 23:49:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+function evenOdd(...numbers)
+{
+    const odd = [];
+    const even = [];
+    for (let i = 0; i < numbers.length; i++)
+    {
+        numbers[i] % 2 === 0? even.push(numbers[i]) : odd.push(numbers[i]);
+    }
+
+    function formatArray(evenOdd)
+    {
+        return evenOdd.sort().toString().replaceAll(',', ', ');
+    }
+
+    return `
+    Odds = ${formatArray(odd)} | Evens = ${formatArray(even)}
+    `
+}
+
+console.log(evenOdd(1, 2, 3, 4, 5));
+
+
+
+
+/* 
+OLD CODE. 
 const numeros = [10, 5, 20, 21, 22, 24, 25]; 
 const impares = [];
 const pares = [];
@@ -22,4 +48,4 @@ function parImpar(){
 }
 
 console.log(parImpar());
-
+*/
