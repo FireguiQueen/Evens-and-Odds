2cbe6e0a73a622279a521908cd4116c0b7673ab5
Guilherme <98475125+FireguiQueen@users.noreply.github.com>
2024-04-16 16:09:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,31 +21,3 @@ console.log(evenOdd(1, 2, 3, 4, 5));
 
 
 
-
-/* 
-OLD CODE. 
-const numeros = [10, 5, 20, 21, 22, 24, 25]; 
-const impares = [];
-const pares = [];
-
-function parImpar(){
-    for(let i = 0; i < numeros.length; i++){
-        if(numeros[i] % 2 === 0){
-            pares.push(numeros[i]);
-        } else {
-            impares.push(numeros[i]);
-        }
-    }
-
-    function formatArray(parImpar){
-        return parImpar.sort().toString().replaceAll(',', ', ')
-    }
-
-    return `
-        Ímpares = ${formatArray(impares)}
-        Pares = ${formatArray(pares)}
-    `
-}
-
-console.log(parImpar());
-*/
